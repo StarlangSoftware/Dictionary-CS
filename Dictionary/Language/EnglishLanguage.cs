94127c87db78ed9cef752b4c1f11dcae4dd8c446
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-02-10 10:28:02 +0300

    Added EnglishLanguage class.

---
+++ Dictionary/Language/EnglishLanguage.cs
@@ -0,0 +1,8 @@
+namespace Dictionary.Language
+{
+    public class EnglishLanguage
+    {
+        public static string LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+        public static string UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+}
